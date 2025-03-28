0b20afaf0b8badf248728858681b3a924025e54e
Tshering00 <tshering.work1@gmail.com>
2025-03-28 16:34:52 +0545

    customized the user form and added extra fields like first name, last name and email field

---
+++ todolist/users/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class RegisterForm(UserCreationForm):
+    username = forms.CharField(max_length=50)
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    email = forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
--- todolist/users/views.py
+++ todolist/users/views.py
@@ -4,10 +4,11 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.urls import reverse_lazy
+from users.forms import RegisterForm
 # Create your views here.
 
 class RegisterView(CreateView):
-    form_class = UserCreationForm
+    form_class = RegisterForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
     
